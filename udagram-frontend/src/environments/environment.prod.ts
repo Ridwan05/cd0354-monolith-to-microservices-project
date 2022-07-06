b2986c1716152f03758acfdb13c95ab8bd7db6b2
Ridwan Oladele <ridohlah74@gmail.com>
2022-07-06 15:38:06 +0100

    complete project upload

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad39a59788b35487a8a4b193e47b3e13-261184223.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
